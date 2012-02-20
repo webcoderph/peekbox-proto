1d0a58f6b758b580a6259faa48ee262e6c5a7379
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-02-20 13:55:39 +0800

    link to host + email setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,20 @@ Peekbox::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+	config.action_mailer.perform_deliveries = true
+
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.default :charset => "utf-8"
+
+	config.action_mailer.smtp_settings = {
+		:address => "smtp.gmail.com",
+		:port => 587,
+		:authentication => :plain,
+		:domain => ENV['GMAIL_SMTP_USER'],
+		:user_name => ENV['GMAIL_SMTP_USER'],
+		:password => ENV['GMAIL_SMTP_PASSWORD'],
+		:enable_starttls_auto => true
+	}
+	config.action_mailer.default_url_options = { :host => 'gentle-beach-4013.heroku.com' }
 end
