67f6f4e3d355e8ce78505828f8b5d64b346fb189
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-02-20 11:30:09 +0800

    video controller and video new form

--- app/controllers/profile/videos_controller.rb
+++ app/controllers/profile/videos_controller.rb
@@ -5,19 +5,16 @@ class Profile::VideosController < Profile::ProfileController
 	end
 
 	def new
-		@category = Category.all
+		@all_category = Category.all
+		@video = Video.new
 	end
 
 	def show
 	end
 
 	def create
-		@video = Video.new
-		@video.title = params[:title]
-		@video.description = params[:description]
+		@video = Video.new(params[:video])
 		@video.user = current_user
-		@video.category = params[:category_id]
-		@video.inspect
 		if @video.save!
 			redirect_to @video, :notice => "Successfully uploaded a video!"
 		else
