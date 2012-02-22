2ff9dda2984bd6b305d916344a8396fa6a5a8b56
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-02-22 20:34:00 +0800

    albums dashboard controller and views

--- app/controllers/profile/albums_controller.rb
+++ app/controllers/profile/albums_controller.rb
@@ -1,6 +1,21 @@
 class Profile::AlbumsController < Profile::ProfileController 
 
   def index
+		@albums = Album.all
+  end
+
+	def create
+		@album = Album.new(params[:album])
+		@album.user = current_user
+		if @album.save! 
+			redirect_to @album, :notice => "Successfully added an Album!"
+		else
+			render :action => "new"
+		end
+	end
+
+  def new
+		@album = Album.new
   end
 
 end
