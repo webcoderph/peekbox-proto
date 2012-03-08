a8c7c56ec63c1e3f22cd0957d2d8da7fc0992107
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-03-09 04:19:41 +0800

    tmp no more all uploaders

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -17,6 +17,9 @@ class MediaUploader < CarrierWave::Uploader::Base
   end
 
 
+	def cache_dir
+		"#{Rails.root}/tmp/uploads"
+	end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
