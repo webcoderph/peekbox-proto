dc0069932068007b0bb91613cef3c5df52850932
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-06 13:43:46 +0800

    video edit informations

--- app/controllers/profile/videos_controller.rb
+++ app/controllers/profile/videos_controller.rb
@@ -1,12 +1,30 @@
 class Profile::VideosController < Profile::ProfileController
 
 	def index
-		@recent_videos = Dashboard.recent_videos(current_user)
+		@videos = current_user.videos.processed.order("id").page(params[:page])
+		@all_categories = Category.all
 	end
 
 	def new
 		@all_category = Category.all
 		@video = Video.new
+	end
+
+	def edit
+		@all_category = Category.all
+		@video = Video.find_by_id(params[:id])
+	end
+
+	def update 
+		@video = Video.find_by_id(params[:video][:id])
+		@video.title = params[:video][:title]
+		@video.description = params[:video][:description]
+		@video.category = Category.find_by_id(params[:video][:category_id])
+		if @video.save! 
+			redirect_to profile_videos_path, :notice => "Successfully updated a video!"
+		else
+			render :action => "edit"
+		end
 	end
 
 	def create
