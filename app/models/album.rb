10c5904249ebdc68136db9ab3169ca2a7d9382b2
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-06 21:40:22 +0800

    updated attr accessors

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ActiveRecord::Base
-	attr_accessible :album_id, :user_id, :image
+	attr_accessible :album_id, :user_id, :image, :pictures
   belongs_to :user
 	mount_uploader :image, ImageUploader
 	has_many :pictures
