3317c99fdbe769833a6fb2edeeea5b2c9316da57
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-06 10:34:41 +0800

    editing of events page

--- app/controllers/profile/events_controller.rb
+++ app/controllers/profile/events_controller.rb
@@ -18,8 +18,22 @@ class Profile::EventsController < Profile::ProfileController
 		end
 	end
 
+	def update
+		@event = Event.find_by_id(params[:event][:id])
+		@event.title = params[:event][:title]
+		@event.description = params[:event][:description]
+		@event.date = params[:event][:date]
+		@event.location = params[:event][:location]
+		@event.image = params[:event][:image]
+		if @event.save!
+			redirect_to @event, :notice => "Successfully added an Event!"	
+		else
+			render :action => "new"
+		end
+	end
+
 	def edit
-		@user_event = current_user.events.find_by_id(params[:id])
+		@event = current_user.events.find_by_id(params[:id])
 	end
 
 end
