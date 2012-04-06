f06e8e66d55a883189ecddf55b70ed5d68a7a491
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-06 21:54:07 +0800

    make album pictures attr accessible

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ActiveRecord::Base
 	attr_accessible :album_id, :user_id, :image, :pictures
   belongs_to :user
-	mount_uploader :image, ImageUploader
 	has_many :pictures
+	mount_uploader :image, ImageUploader
 end
