10ec5834800b29ef187fe19247958d81144648c1
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-09 03:01:43 +0800

    ability to add ads

--- app/controllers/admin/ads_controller.rb
+++ app/controllers/admin/ads_controller.rb
@@ -16,6 +16,16 @@ class Admin::AdsController < ApplicationController
   end
 
   def create
+		@ads = Ads.create
+		@ads.name = params[:ads][:name]
+		@ads.url = params[:ads][:url]
+		@ads.location = params[:ads][:location]
+		@ads.banner = params[:ads][:banner]
+		if @ads.save!
+			redirect_to admin_ads_path, :notice => "Successfully added an Ad"
+		else
+			redirect_to new_admin_ad_path, :alert => "Failed to Add an Ad"
+		end
   end
 
   def update
--- app/models/ads.rb
+++ app/models/ads.rb
@@ -1,6 +1,6 @@
 class Ads < ActiveRecord::Base
 	has_many :ads_state
 
-	mount_uploader :adsBannerImage, AdsUploader
+	mount_uploader :banner, AdsUploader
 	
 end
