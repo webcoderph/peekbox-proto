94f1d00b57b5e187b4bfd8743b482a61b3e7ae83
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-09 20:55:48 +0800

    home ads

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
 		@videos = Video.processed.limit(3)
 		@events = Event.limit(3)
-		@ad = Ads.find_by_location("home")
+		@ad ||= Ads.find_by_location("home")
   end
 
 end
