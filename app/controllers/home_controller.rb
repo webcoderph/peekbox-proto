31cbb6fa748a657052c3973129d5fe2d6eaa2c09
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-11 16:21:09 +0800

    banners at the home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+		@banners = Banner.all
 		@videos = Video.processed.limit(3)
 		@events = Event.limit(3)
 		@ad ||= Ads.find_by_location("home")
