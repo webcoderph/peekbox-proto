dc08799357acc4e247f730abcde888e248ae7d25
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-14 16:01:12 +0800

    category page

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,6 +3,7 @@ class CategoryController < ApplicationController
 		@all_categories = Category.all
 		@videos = Category.find_by_id(params[:id]).videos.page(params[:page])
 		@title = Category.find_by_id(params[:id])
+		@ad ||= Ads.find_by_location("videos")
   end
 
 end
