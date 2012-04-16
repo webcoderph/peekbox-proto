034435f2c0a6c11f04ae537db52a90126a33d571
Maryon Mayor <mayor_maryon@yahoo.com.ph>
2012-04-17 00:57:37 +0800

    look and feel for dashboard pt1

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,10 +1,18 @@
 class Admin::AdminController < ApplicationController
 	before_filter :authenticate_user!
 	before_filter :check_admin!
+	before_filter :banned_user!
 
 	def check_admin!
 		unless current_user.admin
 			redirect_to destroy_user_session_url, :method => :delete
 		end
+	end
+
+	def banned_user!
+		unless current_user.banned == true
+			redirect_to destroy_user_session_url, :method => :delete, :alert => "YOU ARE BANNED!"
+		end
+
 	end
 end
--- app/controllers/profile/profile_controller.rb
+++ app/controllers/profile/profile_controller.rb
@@ -1,8 +1,15 @@
 class Profile::ProfileController < ApplicationController
 	before_filter :authenticate_user!
+	before_filter :banned_user!
 
 	def index
 		@user = current_user
+	end
+
+	def banned_user!
+		unless current_user.banned == true
+			redirect_to destroy_user_session_url, :method => :delete, :alert => "YOU ARE BANNED!"
+		end
 	end
 
 end
